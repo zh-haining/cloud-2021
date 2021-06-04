53cf818e1e6fed4ce7723c4c0d2e38fb8b25b3ae
zh_haininig <zh_haining@163.com>
2021-06-04 11:21:20 +0800

    抽离出dao公共层

---
+++ cloud-db-dao/src/main/java/com/snpc/DaoMain.java
@@ -0,0 +1,16 @@
+package com.snpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @desc:
+ * @author: zh_haining
+ * @date: 2021/6/4 上午11:00
+ */
+@SpringBootApplication
+public class DaoMain {
+    public static void main(String[] args) {
+        SpringApplication.run(DaoMain.class, args);
+    }
+}
